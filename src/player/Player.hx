9a17df059ba6210a92a4dab616e887348cbad878
Andrew Munro <andrew.munro@skybettingandgaming.com>
2016-05-09 00:30:50 +0100

    Adding jump

--- src/player/Player.hx
+++ src/player/Player.hx
@@ -5,11 +5,15 @@ import nape.shape.Circle;
 import nape.space.Space;
 import nape.phys.Body;
 import nape.phys.BodyType;
+import nape.geom.Vec2;
 import milkshake.math.Vector2;
+import milkshake.components.input.Key;
+import milkshake.components.input.Input;
 import pixi.core.textures.Texture;
 import milkshake.core.Sprite;
 import milkshake.core.DisplayObject;
 import milkshake.utils.Color;
+
 
 class Player extends DisplayObject
 {
@@ -17,10 +21,12 @@ class Player extends DisplayObject
 
 	var sprite:Sprite;
 	var texture:Texture;
+	var input:Input;
 
 	public function new(space:Space)
 	{
 		super('player');
+		input = new Input();
 
 		addNode(sprite = new Sprite(texture = Texture.fromFrame("alienBeige_stand.png")),
 		{
@@ -31,6 +37,8 @@ class Player extends DisplayObject
 		body = new Body(BodyType.DYNAMIC);
 		body.shapes.add(new Circle(10));
 		space.bodies.add(body);
+
+		untyped window.body = body;
 
 		if(milkshake.utils.Globals.DEBUG)
 		{
@@ -58,8 +66,22 @@ class Player extends DisplayObject
 		return super.set_rotation(value);
 	}
 
+	function canJump():Bool
+	{
+		if(body != null)
+		{
+			return body.velocity.y == 0;
+		}
+		return false;
+	}
+
 	override public function update(deltaTime:Float):Void
 	{
+		if(input.isDown(Key.SPACE) && canJump())
+		{
+			body.applyImpulse(body.localVectorToWorld(new Vec2(0, -25)));
+		}
+
 		if(body != null)
 		{
 			x = body.position.x;
